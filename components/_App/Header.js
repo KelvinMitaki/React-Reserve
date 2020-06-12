c2cfd37801800a2b73a2a603eaf623ab409b995c
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-12 09:35:08 +0300

    added header styling

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -1,5 +1,25 @@
+import { Menu, Container, Image } from "semantic-ui-react";
+import Link from "next/link";
+
 function Header() {
-  return <>Header</>;
+  return (
+    <>
+      <Menu fluid inverted id="menu">
+        <Container text>
+          <Link href="/">
+            <Menu.Item header>
+              <Image
+                size="mini"
+                src="/public/logo.svg"
+                style={{ marginRight: "1em" }}
+              />
+              React Reserve
+            </Menu.Item>
+          </Link>
+        </Container>
+      </Menu>
+    </>
+  );
 }
 
 export default Header;
